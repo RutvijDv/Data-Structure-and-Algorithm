2add0b10595014313cb138729029f50ca070e73f
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-05-28 02:30:29 +0530

    rat with multipl jumps

---
+++ Geeks For Geeks/Backtracking/Rat with multiple jumps.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSafe(vector<int> maze[], int i, int j, int N)
+{
+    return i < N && j < N && maze[i][j] != 0;
+}
+
+bool solve(vector<int> maze[], int n, int x, int y, vector<vector<int>> &sol)
+{
+    if (x == n - 1 && y == n - 1)
+    {
+        sol[x][y] = 1;
+        return true;
+    }
+
+    if (isSafe(maze, x, y, n))
+    {
+        int jump = maze[x][y];
+        sol[x][y] = 1;
+
+        for (int k = 1; k <= jump; k++)
+        {
+            if (solve(maze, n, x, y + k, sol))
+                return true;
+            if (solve(maze, n, x + k, y, sol))
+                return true;
+        }
+
+        sol[x][y] = 0;
+    }
+
+    return false;
+}
+
+void solve(int N, vector<int> maze[])
+{
+    vector<vector<int>> sol(N, vector<int>(N, 0));
+    if (!solve(maze, N, 0, 0, sol))
+    {
+        cout << "-1" << endl;
+        return;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << sol[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return;
+}
