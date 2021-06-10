8aa67edbcca36481f8cb97ac6d9ff135dd4119aa
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-06-10 23:55:56 +0530

    Nth node from last

--- CTCI/LinkedList/2.2.cpp
+++ CTCI/LinkedList/2.2.cpp
@@ -6,3 +6,52 @@ struct Node
     Node *next = NULL;
     int data;
 };
+
+//Iterative
+
+int getNthFromLast(Node *head, int n)
+{
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL && n--)
+    {
+        fast = fast->next;
+    }
+
+    if (n > 0)
+        return -1;
+
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    return slow->data;
+}
+
+//Recursive
+
+int solve(Node *root, int &res, int &n)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int count = solve(root->next, res, n) + 1;
+
+    if (count == n)
+        res = root->data;
+
+    return count;
+}
+
+int getNthFromLast(Node *head, int n)
+{
+    int res = -1;
+    solve(head, res, n);
+
+    return res;
+}
