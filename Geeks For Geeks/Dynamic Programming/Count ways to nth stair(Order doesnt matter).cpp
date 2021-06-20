98edbc020c6f5e8d3580c22e3b66bd79ad250f8e
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-06-20 12:22:35 +0530

    DP

--- Geeks For Geeks/Dynamic Programming/Count ways to nth stair(Order doesnt matter).cpp
+++ Geeks For Geeks/Dynamic Programming/Count ways to nth stair(Order doesnt matter).cpp
@@ -41,3 +41,35 @@ long long countWays(int n)
 
     return dp[n];
 }
+
+//OR
+long long countWays(int n)
+{
+    vector<long long int> dp(n + 1, 0);
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int j = 2; j <= n; j++)
+    {
+        dp[j] = (dp[j - 2] + 1);
+    }
+
+    return dp[n];
+}
+
+//OR
+long long countWays(int n)
+{
+    vector<long long int> dp(n + 1, 0);
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int j = 2; j <= n; j++)
+    {
+        dp[j] = (dp[j - 2] + 1);
+    }
+
+    return dp[n];
+}
--- Geeks For Geeks/Dynamic Programming/Count ways to stairs (Order matter).cpp
+++ Geeks For Geeks/Dynamic Programming/Count ways to stairs (Order matter).cpp
@@ -21,3 +21,38 @@ int countWays(int n)
 {
     return (solve(n) % 1000000007);
 }
+
+//Using DP
+int countWays(int n)
+{
+    vector<int> dp(n + 1, 0);
+
+    dp[0] = 1;
+
+    for (int j = 1; j <= n; j++)
+    {
+        if (j >= 2)
+            dp[j] = (dp[j] + dp[j - 2] + dp[j - 1]) % 1000000007;
+        else
+            dp[j] = (dp[j] + dp[j - 1]) % 1000000007;
+    }
+
+    return dp[n];
+}
+
+//OR
+int countWays(int n)
+{
+    int a = 1;
+    int b = 1;
+    int temp;
+
+    for (int j = 2; j <= n; j++)
+    {
+        temp = (b + a) % 1000000007;
+        b = a;
+        a = temp;
+    }
+
+    return a;
+}
