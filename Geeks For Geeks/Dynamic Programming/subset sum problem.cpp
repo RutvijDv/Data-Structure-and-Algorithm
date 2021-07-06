07bfb285778c9ee9197ded6ca97943f20fddd1b5
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-07-06 13:07:09 +0530

    DP

--- Geeks For Geeks/Dynamic Programming/subset sum problem.cpp
+++ Geeks For Geeks/Dynamic Programming/subset sum problem.cpp
@@ -2,32 +2,78 @@
 using namespace std;
 
 //Brute force -> Recursive
-bool solve(int a[], int i, int grp, int sum)
+bool solve(int a[], int sum, int i, int n)
 {
-    if (i == 0)
-    {
-        if (grp * 2 == sum)
-            return true;
-        else
-            return false;
-    }
+    if (sum == 0)
+        return true;
+    if (i == n)
+        return false;
 
-    return (solve(a, i - 1, grp - a[i - 1], sum) || solve(a, i - 1, grp, sum));
+    int num = a[i];
+
+    if (sum >= num)
+        return (solve(a, sum - num, i + 1, n) || solve(a, sum, i + 1, n));
+    else
+        return solve(a, sum, i + 1, n);
 }
 
 bool findPartition(int a[], int n)
 {
     int sum = 0;
-
     for (int i = 0; i < n; i++)
+    {
         sum += a[i];
+    }
 
-    return solve(a, n, sum, sum);
+    if (sum % 2 == 1)
+        return false;
+    else
+    {
+        return solve(a, sum / 2, 0, n);
+    }
 }
 
 //Dp Solution -> Memoisation
 
 //Dp Solution -> Tabulation
+bool findPartition(int a[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+    }
+
+    if (sum % 2 == 1)
+        return false;
+    else
+    {
+        sum = sum / 2;
+        bool dp[n + 1][sum + 1];
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= sum; j++)
+            {
+                int num = a[i - 1];
+
+                if (j == 0)
+                    dp[i][j] = true;
+                else if (i == 0)
+                    dp[i][j] = false;
+                else if (j >= num)
+                    dp[i][j] = (dp[i - 1][j - num] || dp[i - 1][j]);
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[n][sum];
+    }
+}
+
+//OR
+
 bool findPartition(int a[], int n)
 {
     int sum = 0;
