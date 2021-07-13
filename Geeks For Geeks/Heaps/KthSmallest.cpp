2460492bf3dddc494ba5b994710c2368af05e447
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-07-13 16:53:22 +0530

    heap

--- Geeks For Geeks/Heaps/KthSmallest.cpp
+++ Geeks For Geeks/Heaps/KthSmallest.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int KthLargest(int arr[], int n, int k)
+int kthSmallest(int arr[], int n, int k)
 {
-    priority_queue<int, vector<int>, greater<int>> pq;
+    priority_queue<int> pq;
 
     for (int i = 0; i < n; i++)
     {
@@ -14,7 +14,7 @@ int KthLargest(int arr[], int n, int k)
         }
         else
         {
-            if (arr[i] > pq.top())
+            if (arr[i] < pq.top())
             {
                 pq.pop();
                 pq.push(arr[i]);
