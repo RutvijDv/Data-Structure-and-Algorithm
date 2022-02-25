f8f2ed2636e2ee8315692917fc45cf13ae1fd085
Rutvij Vamja <rutvij.vamja@gmail.com>
2022-02-25 11:57:16 +0530

    codeforces problem

---
+++ Striver's CP Sheet/116-A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define read(type) readInt<type>() // Fast read
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+#define umap unordered_map;
+#define uset unordered_set;
+ll MOD = 100000007;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int cap = 0;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        cap = cap - a + b;
+
+        ans = max(ans, cap);
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast_cin();
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    solve();
+    return 0;
+}
---
+++ Striver's CP Sheet/977-A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define read(type) readInt<type>() // Fast read
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+#define umap unordered_map;
+#define uset unordered_set;
+ll MOD = 100000007;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++)
+    {
+        if (n % 10 == 0)
+        {
+            n = n / 10;
+        }
+        else
+        {
+            n = n - 1;
+        }
+    }
+
+    cout << n << endl;
+}
+
+int main()
+{
+    fast_cin();
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    solve();
+    return 0;
+}
