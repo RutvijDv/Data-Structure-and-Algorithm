333d01ac6d2e51109d5055b4d3bc4227fbb323bf
Rutvij Vamja <rutvij.vamja@gmail.com>
2022-02-26 16:34:14 +0530

    codeforces problem

---
+++ Striver's CP Sheet/118-A.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define read(type) readInt<type>() // Fast read
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+#define umap unordered_map;
+#define uset unordered_set;
+ll MOD = 100000007;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    int i = 0;
+
+    while (i < s.size())
+    {
+        char x = s[i];
+
+        if (x == 'a' || x == 'A' || x == 'e' || x == 'E' || x == 'i' || x == 'I' || x == 'o' || x == 'O' || x == 'u' || x == 'U' || x == 'y' || x == 'Y')
+        {
+            s.erase(i, 1);
+        }
+        else
+        {
+            s.insert(i, ".");
+            i++;
+            if (x < 95)
+            {
+                s[i] = s[i] + 32;
+            }
+
+            i++;
+        }
+    }
+
+    cout << s << endl;
+}
+
+int main()
+{
+    fast_cin();
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    solve();
+
+    return 0;
+}
