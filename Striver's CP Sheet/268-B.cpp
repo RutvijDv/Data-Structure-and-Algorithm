a4aa36fffef9fa335321fe189b04fe39a981c366
Rutvij Vamja <rutvij.vamja@gmail.com>
2022-02-28 13:26:15 +0530

    codeforces problem

---
+++ Striver's CP Sheet/268-B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define read(type) readInt<type>() // Fast read
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+#define umap unordered_map;
+#define uset unordered_set;
+ll MOD = 100000007;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    int ans = 0;
+
+    for (int i = n; i > 0; i--)
+    {
+        ans += i + (i - 1) * cnt;
+        cnt++;
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast_cin();
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    solve();
+    return 0;
+}
