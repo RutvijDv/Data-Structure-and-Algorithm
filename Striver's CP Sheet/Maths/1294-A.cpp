46d8fc7eca5761c5f75bbf86860d27fdac0c973f
Rutvij Vamja <rutvij.vamja@gmail.com>
2022-03-04 16:58:43 +0530

    codeforces problem

---
+++ Striver's CP Sheet/Maths/1294-A.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define read(type) readInt<type>() // Fast read
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+#define umap unordered_map;
+#define uset unordered_set;
+ll MOD = 100000007;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    int a, b, c, n;
+    cin >> a >> b >> c >> n;
+
+    int m = max(a, max(b, c));
+
+    int s = (m - a) + (m - b) + (m - c);
+
+    if (n >= s)
+    {
+        n -= s;
+        if (n % 3 == 0)
+            cout << "YES"
+                 << "\n";
+        else
+            cout << "NO"
+                 << "\n";
+    }
+    else
+    {
+        cout << "NO"
+             << "\n";
+    }
+}
+
+int main()
+{
+    fast_cin();
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
