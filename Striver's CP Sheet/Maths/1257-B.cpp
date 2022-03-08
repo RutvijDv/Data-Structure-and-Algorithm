13e6bd47065cfc2c65e013d37af1886dd90db040
Rutvij Vamja <rutvij.vamja@gmail.com>
2022-03-08 12:54:50 +0530

    codeforces problem

---
+++ Striver's CP Sheet/Maths/1257-B.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define read(type) readInt<type>() // Fast read
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+#define umap unordered_map;
+#define uset unordered_set;
+ll MOD = 100000007;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+
+    if (x == 1)
+    {
+        if (y == 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+
+        return;
+    }
+
+    if ((x == 3 && y > 3) || (x == 2 && y > 3))
+    {
+        cout << "NO\n";
+        return;
+    }
+
+    cout << "YES\n";
+}
+
+int main()
+{
+    fast_cin();
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
